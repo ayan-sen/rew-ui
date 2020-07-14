9427f4aca8ead8142e50c12d62283e9b3db1ce69
Ayan Sen <ayan.sen@live.com>
2020-07-14 12:59:29 +0530

    login implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { OrderPlacementComponent } from './transaction/order-placement/order-placement.component';
-import { OrderPlacementShowComponent } from './transaction/order-placement/order-placement-show/order-placement-show.component';
-import { ProjectComponent } from './transaction/project/project.component';
-import { ProjectShowComponent } from './transaction/project/project-show/project-show.component';
-import { OrderDeliveryComponent } from './transaction/order-delivery/order-delivery.component';
-import { OrderDeliveryShowComponent } from './transaction/order-delivery/order-delivery-show/order-delivery-show.component';
-import { OrderProcessingComponent } from './transaction/order-processing/order-processing.component';
-import { OrderProcessingShowComponent } from './transaction/order-processing/order-processing-show/order-processing-show.component';
-import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './components/common-service/auth-guard.service';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
  
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full'  },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuardService ] },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  
+  { path: '', redirectTo: '', pathMatch: 'full', canActivate: [ AuthGuardService ]   },
+  { path: '', 
+    component: AdminLayoutComponent,
+    canActivate: [ AuthGuardService ],
+    children : [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full'  },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'transaction', loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule) }
+    ]  
+  },
+  { path: 'login', component: LoginComponent },
+  
+  
 ];
  
 @NgModule({
