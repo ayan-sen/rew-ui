e3c34276b8dbf5eafdad32b14b601f30861d5c4d
Ayan Sen <ayan.sen@live.com>
2022-08-09 12:45:52 +0530

    modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://rew-portal.ap-south-1.elasticbeanstalk.com/rew-portal'
+  // baseUrl: 'http://rew-portal.ap-south-1.elasticbeanstalk.com/rew-portal'
+  baseUrl: 'http://localhost:8080/rew-portal'
+  
 };
