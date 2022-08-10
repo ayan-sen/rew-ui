6e4614e23c00d791cd4d42e44518a48305bd6441
Ayan Sen <ayan.sen@live.com>
2022-08-10 22:47:18 +0530

    changed prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://rew-portal.ap-south-1.elasticbeanstalk.com/rew-portal'
-  baseUrl: 'http://localhost:8080/rew-portal'
+  baseUrl: 'http://desolate-gorge-62749.herokuapp.com/rew-portal'
   
 };
